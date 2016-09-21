2f0db87e50d86837b1a1dd80254e95d8dd05550d
Valentin Butakov <vbutakov@gmail.com>
2016-09-21 18:54:01 +0300

    added create user page tests

---
+++ src/yorablog/db_test.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"errors"
+	"time"
+	"yoradb"
+)
+
+type tDB struct {
+}
+
+func (db *tDB) DBInsertPost(post *yoradb.Post, userID int) (int, error) {
+	return 10, nil
+}
+
+func (db *tDB) DBGetPostByID(id int) (*yoradb.Post, error) {
+	p := &yoradb.Post{
+		ID:          id,
+		Title:       "Тестовый заголовок",
+		Description: "Тестовое описание",
+		ImageURL:    "http://example.com/image.jpg",
+		Annotation:  "Тестовая аннотация",
+		Text:        "Тестовый текст",
+		Author:      "Тестовый автор",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	return p, nil
+}
+
+func (db *tDB) DBUpdatePost(post *yoradb.Post) error {
+	return nil
+}
+
+func (db *tDB) DBGetPosts(num, offset int) ([]yoradb.Post, error) {
+	return nil, nil
+}
+
+func (db *tDB) DBGetUserBySessionID(sessionID string) (*yoradb.User, error) {
+
+	if user, ok := sessions[sessionID]; ok {
+		return user, nil
+	} else {
+		return nil, errors.New("User not found")
+	}
+}
+
+func (db *tDB) DBSessionValid(sessionID string) bool {
+	return true
+}
+
+func (db *tDB) DBInsertNewSession(sessionID string, expires time.Time) error {
+	return nil
+}
+
+func (db *tDB) DBUserIsLogedIn(sessionID string) bool {
+	return true
+}
+
+func (db *tDB) DBCreateUser(name, email, password string) (int, error) {
+	return 10, nil
+}
+
+func (db *tDB) DBUpdateSessionWithUserID(sessionID string, userID int) error {
+	return nil
+}
+
+func (db *tDB) DBLoginUser(email, password string) (int, error) {
+	return 10, nil
+}
+
+func (db *tDB) DBLogoutUserFromSession(sessionID string) error {
+	return nil
+}
+
+func (db *tDB) DBEmailExist(email string) bool {
+	return true
+}
+
+func (db *tDB) DBCreateRestorePasswordID(email, token string) (string, error) {
+	return "", nil
+}
+
+func (db *tDB) DBGetEmailByRestoreToken(token string) (string, error) {
+	return "", nil
+}
+
+func (db *tDB) DBUpdatePasswordByRestoreToken(token, email, password string) error {
+	return nil
+}
+
+func (db *tDB) Close() error {
+	return nil
+}
--- src/yorablog/post_test.go
+++ src/yorablog/post_test.go
@@ -4,103 +4,8 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"os"
-	"strings"
 	"testing"
-	"time"
-	"yoradb"
 )
-
-type tDB struct {
-}
-
-func (db *tDB) DBInsertPost(post *yoradb.Post, userID int) (int, error) {
-	return 10, nil
-}
-
-func (db *tDB) DBGetPostByID(id int) (*yoradb.Post, error) {
-	p := &yoradb.Post{
-		ID:          id,
-		Title:       "Тестовый заголовок",
-		Description: "Тестовое описание",
-		ImageURL:    "http://example.com/image.jpg",
-		Annotation:  "Тестовая аннотация",
-		Text:        "Тестовый текст",
-		Author:      "Тестовый автор",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	return p, nil
-}
-
-func (db *tDB) DBUpdatePost(post *yoradb.Post) error {
-	return nil
-}
-
-func (db *tDB) DBGetPosts(num, offset int) ([]yoradb.Post, error) {
-	return nil, nil
-}
-
-func (db *tDB) DBGetUserBySessionID(sessionID string) (*yoradb.User, error) {
-	user := &yoradb.User{}
-	user.ID = 10
-	user.Name = "Begemotina"
-	user.Email = "vozhdyara@gmail.com"
-	user.CreatePostPermit = true
-	user.EditPostPermit = true
-
-	return user, nil
-}
-
-func (db *tDB) DBSessionValid(sessionID string) bool {
-	return true
-}
-
-func (db *tDB) DBInsertNewSession(sessionID string, expires time.Time) error {
-	return nil
-}
-
-func (db *tDB) DBUserIsLogedIn(sessionID string) bool {
-	return true
-}
-
-func (db *tDB) DBCreateUser(name, email, password string) (int, error) {
-	return 10, nil
-}
-
-func (db *tDB) DBUpdateSessionWithUserID(sessionID string, userID int) error {
-	return nil
-}
-
-func (db *tDB) DBLoginUser(email, password string) (int, error) {
-	return 10, nil
-}
-
-func (db *tDB) DBLogoutUserFromSession(sessionID string) error {
-	return nil
-}
-
-func (db *tDB) DBEmailExist(email string) bool {
-	return true
-}
-
-func (db *tDB) DBCreateRestorePasswordID(email, token string) (string, error) {
-	return "", nil
-}
-
-func (db *tDB) DBGetEmailByRestoreToken(token string) (string, error) {
-	return "", nil
-}
-
-func (db *tDB) DBUpdatePasswordByRestoreToken(token, email, password string) error {
-	return nil
-}
-
-func (db *tDB) Close() error {
-	return nil
-}
 
 func TestPostPageServeHTTP(t *testing.T) {
 	db := &tDB{}
@@ -110,7 +15,7 @@ func TestPostPageServeHTTP(t *testing.T) {
 
 	c := &http.Cookie{}
 	c.Name = "SessionID"
-	c.Value = "1234567890"
+	c.Value = "01"
 
 	req.Header.Set("Cookie", c.String())
 
@@ -135,42 +40,4 @@ func TestPostPageServeHTTP(t *testing.T) {
 	if !bytes.Contains(w.Body.Bytes(), []byte("Тестовый автор")) {
 		t.Errorf("Автор не выведен")
 	}
-}
-
-func TestCreatePageServeHTTP(t *testing.T) {
-	db := &tDB{}
-	h := InitCreatePageHandler(db, "/home/valya/myprogs/yorablog/templates")
-
-	form := &url.Values{}
-	form.Add("title", "Тестовый заголовок")
-	form.Add("description", "Тестовое описание")
-	form.Add("imageurl", "Тестовая картинка")
-	form.Add("annotation", "Тестовая аннотация")
-	form.Add("posttext", "Тестовый текст")
-
-	body := strings.NewReader(form.Encode())
-
-	req := httptest.NewRequest("POST", "http://localhost/create", body)
-	w := httptest.NewRecorder()
-
-	c := &http.Cookie{}
-	c.Name = "SessionID"
-	c.Value = "1234567890"
-
-	req.Header.Set("Cookie", c.String())
-
-	h.ServeHTTP(w, req)
-
-	if w.Code != http.StatusSeeOther {
-		t.Errorf("Wrong error code: %v\n", w.Code)
-	}
-	loc := w.Header().Get("Location")
-	if !strings.Contains(loc, "/post/10") {
-		t.Errorf("Wrong redirect: %v\n", loc)
-	}
-}
-
-func TestMain(m *testing.M) {
-	InitURLPatterns()
-	os.Exit(m.Run())
 }
