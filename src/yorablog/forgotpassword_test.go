e2e5f134e950c3d13bc06103d8a523b105b032d1
Valentin Butakov <vbutakov@gmail.com>
2016-09-29 16:08:13 +0300

    added test for forget password page

---
+++ src/yorablog/forgotpassword_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestForggetPasswordServeHTTP(t *testing.T) {
+	db := &tDB{}
+	h := InitForgotPasswordPageHandler(db, "/home/valya/myprogs/yorablog/templates")
+	req := httptest.NewRequest("GET", "http://localhost/forgotpassword/", nil)
+	w := httptest.NewRecorder()
+
+	c := &http.Cookie{}
+	c.Name = "SessionID"
+	c.Value = "01"
+
+	req.Header.Set("Cookie", c.String())
+
+	h.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Wrong error code: %v\n", w.Code)
+	}
+
+	if !bytes.Contains(w.Body.Bytes(), []byte("</html>")) {
+		t.Errorf("Page is not complete")
+	}
+}
